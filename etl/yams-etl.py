a8fc1823123502341a0b3aaca40f235be62a7622
Mark Wong <mark.wong@myemma.com>
2011-01-24 11:31:02 -0800

    async etl
    Added a command line argument to specify the number of worker threads.
    Using only 1 thread to load the data into couchdb; testing indicates
    that 99% of the 11 minutes required to load data from collectd into
    couchdb is spent loading data into couchdb.  HTTP POSTing 1028 objects
    to the ETL takes approximately 3 seconds, while POSTing the resulting
    13,756 JSON objects to couchdb takes 11 minutes.

--- etl/yams-etl.py
+++ etl/yams-etl.py
@@ -12,12 +12,79 @@ from cgi import parse_qs
 import simplejson as json
 from urllib import urlencode
 from httplib import HTTPConnection
+from threading import Lock, Thread
+from time import sleep
+
+class MyQueue:
+    def __init__(self, verbose):
+        self.verbose = verbose
+
+        self.count = 0
+        self.lock = Lock()
+        self.queue = list()
+
+    def dequeue(self):
+        self.lock.acquire()
+        try:
+            if self.count > 0:
+                data = self.queue.pop(0)
+                self.count -= 1
+                if self.verbose:
+                    print 'dequeue: %d' % self.count
+            else:
+                data = None
+        finally:
+            self.lock.release()
+        return data
+
+    def enqueue(self, data):
+        self.lock.acquire()
+        try:
+            self.queue.append(data)
+            self.count += 1
+            if self.verbose:
+                print 'enqueue: %d' % self.count
+        finally:
+            self.lock.release()
+
+class MyThread(Thread):
+    def __init__(self, queue, hostname, port, uri, verbose):
+        Thread.__init__(self)
+
+        self.queue = queue
+        self.hostname = hostname
+        self.port = port
+        self.uri = uri
+        self.verbose = verbose
+
+    def run(self):
+        while True:
+            data = self.queue.dequeue()
+            if data is None:
+                # Sleep only if there is nothing to do.
+                sleep(5)
+            else:
+                jdata = json.loads(data)
+                if self.verbose:
+                    print json.dumps(jdata, sort_keys=True, indent=4)
+                headers = {'Content-Type': 'application/json'}
+
+                conn = HTTPConnection(self.hostname, self.port)
+                # Since we have a python list of json objects, iterate through
+                # the list and send each python object individually.
+                for datum in jdata:
+                    params = json.dumps(datum)
+                    if self.verbose:
+                        conn.set_debuglevel(1)
+                    conn.request('POST', self.uri, params, headers)
+                    # Must read the response before reusing the connection to
+                    # send another request.
+                    response = conn.getresponse()
+                    response.read()
+                conn.close()
 
 class MyHandler(BaseHTTPRequestHandler):
-    hostname = None
-    port = None
-    uri = None
-    verbose = None
+    queue = None
 
     def do_HEAD(self):
         self.send_response(200)
@@ -35,25 +102,7 @@ class MyHandler(BaseHTTPRequestHandler):
         # datastructure in python is effectively a list of valid json objects,
         # as opposed to a single json object.
         data = self.rfile.read(length)
-
-        jdata = json.loads(data)
-        if self.verbose:
-            print json.dumps(jdata, sort_keys=True, indent=4)
-        headers = {'Content-Type': 'application/json'}
-
-        conn = HTTPConnection(self.hostname, self.port)
-        # Since we have a python list of json objects, iterate through the list
-        # and send each python object individually.
-        for datum in jdata:
-            params = json.dumps(datum)
-            if self.verbose:
-                conn.set_debuglevel(1)
-            conn.request('POST', self.uri, params, headers)
-            # Must read the response before reusing the connection to send
-            # another request.
-            response = conn.getresponse()
-            response.read()
-        conn.close()
+        self.queue.enqueue(data)
 
 def main():
     parser = OptionParser(usage="usage: %prog [options]")
@@ -63,6 +112,7 @@ def main():
     parser.add_option('-u', '--uri', help='uri (default /collectd/)')
     parser.add_option('-v', '--verbose', action='store_true', default=False,
                       help='verbose output')
+    parser.add_option('-w', '--workers', help='number of threads (default 1)')
     options, args = parser.parse_args()
 
     listener = 8888
@@ -85,15 +135,26 @@ def main():
     if options.listener:
         listener = options.listener
 
+    if options.workers:
+        workers = int(options.workers)
+    else:
+        workers = 1
+
     try:
+        queue = MyQueue(verbose)
+
         myh = MyHandler
-        myh.hostname = hostname
-        myh.port = port
-        myh.uri = uri
-        myh.verbose = verbose
+        myh.queue = queue
+
+        i = 0
+        print 'ramping up worker threads'
+        while i < workers:
+            MyThread(queue, hostname, port, uri, verbose).start()
+            i += 1
+        print 'worker threads ramped up'
 
         httpserver = HTTPServer(('', listener), myh)
-        print 'yams test listener started'
+        print 'listener started'
         httpserver.serve_forever()
     except KeyboardInterrupt:
         print '^C received, shutting down server'
