412690adb761de7aaa8fa26d66fae6174d8dcb16
Mark Wong <mark.wong@myemma.com>
2011-01-26 05:01:47 -0800

    add sqlalchemy support to pylons

--- yams-wui/yamswui/websetup.py
+++ yams-wui/yamswui/websetup.py
@@ -4,6 +4,7 @@ import logging
 import pylons.test
 
 from yamswui.config.environment import load_environment
+from yamswui.model.meta import Session, Base
 
 log = logging.getLogger(__name__)
 
@@ -12,3 +13,6 @@ def setup_app(command, conf, vars):
     # Don't reload the app if it was loaded under the testing environment
     if not pylons.test.pylonsapp:
         load_environment(conf.global_conf, conf.local_conf)
+
+    # Create the tables if they don't already exist
+    Base.metadata.create_all(bind=Session.bind)
