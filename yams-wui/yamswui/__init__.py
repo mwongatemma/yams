e43ccb2879c91c9b9938a98ee3e79f0a2a134e97
Mark Wong <mark.wong@myemma.com>
2011-01-27 17:17:01 -0600

    add flotr 0.2.0 alpha
    Also added a static route /js to static/js.

--- yams-wui/yamswui/__init__.py
+++ yams-wui/yamswui/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy import engine_from_config
 
 from yamswui.models import initialize_sql
 
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -10,9 +11,9 @@ def main(global_config, **settings):
     initialize_sql(engine)
     config = Configurator(settings=settings)
     config.add_static_view('static', 'yamswui:static', cache_max_age=3600)
+    config.add_static_view('js', 'yamswui:static/js', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_view('yamswui.views.my_view',
                     route_name='home',
                     renderer='templates/mytemplate.pt')
     return config.make_wsgi_app()
-
