788c55f8d703f77ceac5339c947a1d1e60aadd7a
Mark Wong <mark.wong@myemma.com>
2011-10-20 10:12:43 -0500

    add psycopg req and remove sqlite req

--- yams-wui/setup.py
+++ yams-wui/setup.py
@@ -14,10 +14,10 @@ requires = [
     'pyramid_tm',
     'pyramid_debugtoolbar',
     'zope.sqlalchemy',
+    'waitress',
+    'sqlalchemy',
+    'psycopg2',
     ]
-
-if sys.version_info[:3] < (2,5,0):
-    requires.append('pysqlite')
 
 setup(name='yams-wui',
       version='0.0',
