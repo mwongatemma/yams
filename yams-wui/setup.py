13d83a8cb77f22661b31885326fe4b512060379f
Mark Wong <mark.wong@myemma.com>
2013-04-28 17:58:05 -0700

    Upgrade wui to pyramid 1.4.1

--- yams-wui/setup.py
+++ yams-wui/setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from setuptools import setup, find_packages
 
@@ -14,18 +13,16 @@ requires = [
     'pyramid_tm',
     'pyramid_debugtoolbar',
     'zope.sqlalchemy',
+    'waitress',
     ]
-
-if sys.version_info[:3] < (2,5,0):
-    requires.append('pysqlite')
 
 setup(name='yams-wui',
       version='0.0',
       description='yams-wui',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
-        "Framework :: Pylons",
+        "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
@@ -37,11 +34,11 @@ setup(name='yams-wui',
       include_package_data=True,
       zip_safe=False,
       test_suite='yamswui',
-      install_requires = requires,
-      entry_points = """\
+      install_requires=requires,
+      entry_points="""\
       [paste.app_factory]
       main = yamswui:main
+      [console_scripts]
+      initialize_yams-wui_db = yamswui.scripts.initializedb:main
       """,
-      paster_plugins=['pyramid'],
       )
-
