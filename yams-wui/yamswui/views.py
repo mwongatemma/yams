d064daa3aff654eff0dcbf5b8a9750763c815b0a
Mark Wong <mark.wong@myemma.com>
2013-04-30 17:52:41 -0500

    Hardcoded example to display all system load data
    This is using the d3.js library.

--- yams-wui/yamswui/views.py
+++ yams-wui/yamswui/views.py
@@ -10,4 +10,20 @@ from .models import (
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {}
+    return {'ylabel': ''}
+
+@view_config(route_name='data')
+def my_data(request):
+    session = DBSession()
+    data = session.execute(
+        """SELECT time::TIMESTAMP, values[1] AS shortterm,
+               values[2] AS midterm, values[3] AS longterm
+        FROM value_list
+        WHERE plugin = 'load'
+        ORDER BY time;""")
+
+    resp = 'timestamp,shortterm,midterm,longterm\n'
+    for row in data:
+        resp += '%s,%s,%s,%s\n' % (row[0], row[1], row[2], row[3])
+
+    return Response(resp)
