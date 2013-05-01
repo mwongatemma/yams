50cb586f9a0bef555e522d871902c163dbe79e15
Mark Wong <mark.wong@myemma.com>
2013-05-01 09:42:44 -0500

    Generalize data retrieval view
    This is just a partial generalization to chart all data from all data
    sets for a given plugin.  Only gauge and counter data types are
    currently handled.

--- yams-wui/yamswui/__init__.py
+++ yams-wui/yamswui/__init__.py
@@ -16,6 +16,6 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('data', '/data')
+    config.add_route('data', '/data/{plugin}')
     config.scan()
     return config.make_wsgi_app()
--- yams-wui/yamswui/views.py
+++ yams-wui/yamswui/views.py
@@ -14,16 +14,43 @@ def my_view(request):
 
 @view_config(route_name='data')
 def my_data(request):
+    plugin = request.matchdict['plugin']
+
     session = DBSession()
+
+    # The data source name and type should be the consistent within a plugin.
+    # Grab the first one to get the details.
+    result = session.execute(
+            """SELECT dsnames, dstypes
+            FROM value_list
+            WHERE plugin = :plugin
+            LIMIT 1;""", {'plugin': plugin}).first()
+    dsnames = result[0]
+    dstypes = result[1]
+    length = len(dsnames)
+
+    # Cast the timestamp with time zone to without time zone, which should
+    # result in the system timezone because I can't figure out the format to
+    # make d3 read the time zone correctly.
     data = session.execute(
-        """SELECT time::TIMESTAMP, values[1] AS shortterm,
-               values[2] AS midterm, values[3] AS longterm
-        FROM value_list
-        WHERE plugin = 'load'
-        ORDER BY time;""")
+            """SELECT time::TIMESTAMP, values
+            FROM value_list
+            WHERE plugin = :plugin
+            ORDER BY time;""", {'plugin': plugin})
 
-    resp = 'timestamp,shortterm,midterm,longterm\n'
+    csv = 'timestamp,%s\n' % ','.join(dsnames)
+
+    lastrow = data.fetchone()
     for row in data:
-        resp += '%s,%s,%s,%s\n' % (row[0], row[1], row[2], row[3])
+        datum = []
+        for i in range(length):
+            # TODO: Handle counter and absolute types.
+            if dstypes[i] == 'gauge':
+                datum.append(str(lastrow[1][i]))
+            elif dstypes[i] == 'derive':
+                datum.append(str(lastrow[1][i] - row[1][i]))
 
-    return Response(resp)
+        csv += '%s,%s\n' % (lastrow[0], ','.join(datum))
+        lastrow = row
+
+    return Response(csv)
